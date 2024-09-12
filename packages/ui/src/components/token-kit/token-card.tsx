bd98d97e6ad5dc40b995a23bf7a1d3e13070206d
futantan <luckytantanfu@gmail.com>
2024-09-12 14:33:51 +1000

    feat: 🎸 styling

--- packages/ui/src/components/token-kit/token-card.tsx
+++ packages/ui/src/components/token-kit/token-card.tsx
@@ -1,6 +1,5 @@
 "use client";
-import { Card, CardContent, CardHeader } from "@/components/ui/card";
-import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Skeleton } from "@/components/ui/skeleton";
 import { useQuery } from "@tanstack/react-query";
 import React from "react";
@@ -38,6 +37,28 @@ function TokenCardSkeleton() {
             <Skeleton className="h-4 w-48" />
             <Skeleton className="h-4 w-36" />
           </div>
+        </div>
+      </CardContent>
+    </Card>
+  );
+}
+
+function NFTCardSkeleton() {
+  return (
+    <Card className="w-[350px] overflow-hidden">
+      <CardHeader className="p-0">
+        <Skeleton className="w-full h-[200px]" />
+      </CardHeader>
+      <CardContent className="p-4">
+        <Skeleton className="h-8 w-3/4 mb-2" />
+        <Skeleton className="h-4 w-full mb-4" />
+        <div className="grid grid-cols-2 gap-x-4 gap-y-2">
+          {[...Array(3)].map((_, index) => (
+            <div key={index} className="flex flex-col">
+              <Skeleton className="h-4 w-20 mb-1" />
+              <Skeleton className="h-5 w-28" />
+            </div>
+          ))}
         </div>
       </CardContent>
     </Card>
@@ -98,13 +119,26 @@ function ERC721TokenCard({
   tokenId,
   ...props
 }: ERC721TokenCardProps) {
-  const { data: erc721TokenURI } = useReadContract({
-    chainId,
-    address: contract,
-    abi: erc721Abi,
-    functionName: "tokenURI",
-    args: [BigInt(tokenId)],
+  const { data } = useReadContracts({
+    allowFailure: false,
+    contracts: [
+      {
+        chainId,
+        address: contract,
+        abi: erc721Abi,
+        functionName: "tokenURI",
+        args: [BigInt(tokenId)],
+      },
+      {
+        chainId,
+        address: contract,
+        abi: erc721Abi,
+        functionName: "name",
+      },
+    ],
   });
+
+  const [erc721TokenURI, name] = data || [];
 
   const { data: erc721Metadata } = useQuery({
     queryKey: ["metadata", chainId, contract, tokenId],
@@ -122,11 +156,12 @@ function ERC721TokenCard({
   const attributes = metadata?.attributes;
 
   if (!metadata) {
-    return <TokenCardSkeleton />;
+    return <NFTCardSkeleton />;
   }
 
   return (
     <TokenMetadataDisplay
+      title={name || ""}
       metadata={metadata}
       attributes={attributes}
       {...props}
@@ -151,21 +186,9 @@ export const ERC1155TokenCard: React.FC<ERC1155TokenCardProps> = ({
     address: contract,
     abi: [
       {
-        inputs: [
-          {
-            internalType: "uint256",
-            name: "id",
-            type: "uint256",
-          },
-        ],
+        inputs: [{ internalType: "uint256", name: "id", type: "uint256" }],
         name: "uri",
-        outputs: [
-          {
-            internalType: "string",
-            name: "",
-            type: "string",
-          },
-        ],
+        outputs: [{ internalType: "string", name: "", type: "string" }],
         stateMutability: "view",
         type: "function",
       },
@@ -199,11 +222,12 @@ export const ERC1155TokenCard: React.FC<ERC1155TokenCardProps> = ({
   );
 
   if (!metadata) {
-    return <TokenCardSkeleton />;
+    return <NFTCardSkeleton />;
   }
 
   return (
     <TokenMetadataDisplay
+      title={metadata?.name}
       metadata={metadata}
       attributes={attributes}
       {...props}
@@ -212,63 +236,53 @@ export const ERC1155TokenCard: React.FC<ERC1155TokenCardProps> = ({
 };
 
 interface TokenMetadataDisplayProps {
+  title: string;
   metadata: { image: string; description?: string };
   attributes?: Array<{ trait_type: string; value: string }>;
 }
 
 export const TokenMetadataDisplay: React.FC<TokenMetadataDisplayProps> = ({
+  title,
   metadata,
   attributes,
 }) => {
   return (
-    <Card>
-      <CardHeader className="relative cursor-pointer space-y-0 p-0">
+    <Card className="w-[350px] overflow-hidden">
+      <CardHeader className="p-0">
         <img
-          className="rounded-lg"
+          alt="NFT"
+          className="w-full object-cover"
           src={rewriteUrlIfIpfsUrl(metadata?.image)}
         />
       </CardHeader>
       <CardContent className="p-4">
-        <div className="flex flex-col gap-4">
-          <div className="relative w-full">
-            <h3 className="mb-2 text-lg font-semibold leading-none">
-              Description
-            </h3>
-            <p className="text-muted-foreground text-sm">
-              {metadata?.description}
-            </p>
-          </div>
+        <CardTitle className="text-2xl font-bold">{title}</CardTitle>
+        <p className="text-sm text-muted-foreground mt-2">
+          {metadata?.description}
+        </p>
 
-          <div className="w-full">
-            <h3 className="mb-2 text-lg font-semibold leading-none">Traits</h3>
-            <ScrollArea className="w-full whitespace-nowrap rounded-md border p-2">
-              <div className="flex w-full gap-2">
-                {attributes?.map(({ trait_type, value }) => {
-                  return (
-                    <div
-                      key={trait_type}
-                      className="bg-primary-100/10 flex w-full flex-col items-center rounded-md border"
-                    >
-                      <div className="font-semibold">{trait_type}</div>
-                      {value.toString().indexOf("https://") === 0 ? (
-                        <a
-                          href={value}
-                          target="_blank"
-                          className="text-primary-500 cursor-pointer underline"
-                        >
-                          {shortenUrl(value)}
-                        </a>
-                      ) : (
-                        <div>{formatValueOrAddress(value.toString())}</div>
-                      )}
-                    </div>
-                  );
-                })}
-              </div>
-              <ScrollBar orientation="horizontal" />
-            </ScrollArea>
-          </div>
-        </div>
+        <dl className="grid grid-cols-2 gap-x-4 gap-y-2 mt-4 text-sm">
+          {attributes?.map((item) => (
+            <div key={item.trait_type} className="flex flex-col">
+              <dt className="font-medium text-muted-foreground">
+                {item.trait_type}
+              </dt>
+              <dd className="font-semibold">
+                {item.value.toString().indexOf("https://") === 0 ? (
+                  <a
+                    href={item.value}
+                    target="_blank"
+                    className="text-primary-500 cursor-pointer underline"
+                  >
+                    {shortenUrl(item.value)}
+                  </a>
+                ) : (
+                  <div>{formatValueOrAddress(item.value.toString())}</div>
+                )}
+              </dd>
+            </div>
+          ))}
+        </dl>
       </CardContent>
     </Card>
   );
