43f49c037bcefe30d17b7e0f048fcbe25e16c87c
Justin Feng <realfengjia@foxmail.com>
2024-09-19 09:59:45 +0800

    chore: 🎸 export consts

--- packages/onchain/src/index.ts
+++ packages/onchain/src/index.ts
@@ -1,3 +1,14 @@
+export {
+  TokenType,
+  TokenDataOptions,
+  FetchHandler,
+  ERC20TokenData,
+  ERC721TokenData,
+  ERC1155TokenData,
+  MyNftsOptions,
+  MyNftToken,
+  MyNfts,
+} from "./constant";
 export * from "./libs";
 
 export * from "./token-type";
