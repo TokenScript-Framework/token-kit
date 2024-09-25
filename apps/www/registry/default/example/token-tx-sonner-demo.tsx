1294c7dd2c1aeb1863b5685e48905949f8a149e6
nicktaras <nicktaras@hotmail.co.uk>
2024-09-25 11:48:44 +0100

    feat: 🎸 simplified tx sonner example

--- apps/www/registry/default/example/token-tx-sonner-demo.tsx
+++ apps/www/registry/default/example/token-tx-sonner-demo.tsx
@@ -5,16 +5,12 @@ import {
 import { Button } from "@/components/ui/button";
 
 export default function TokenTxSonnerDemo() {
-  const simulateTx = (txHash: string, txUrl?: string, duration?: number) => {
-    showTxSonner(txHash, txUrl, duration);
-  };
-
   return (
     <div>
       <ToastProvider />
       <Button
         onClick={() =>
-          simulateTx(
+          showTxSonner(
             "0x4ee12786284e8a4a0d2f6e3235db37a96b47d473d67526135895ee80cfb349cb",
             "https://etherscan.io/tx/",
             5000,
