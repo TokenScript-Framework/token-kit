f6f4c4487a5ca1dada9a43cc152bf64f1a57dbca
futantan <luckytantanfu@gmail.com>
2024-09-26 10:38:07 +1000

    refactor: 💡 wagmi-config

--- apps/www/config/wagmi-config.ts
+++ apps/www/config/wagmi-config.ts
@@ -2,15 +2,15 @@ import { createConfig as actionConfig } from "@wagmi/core";
 import { createConfig, http } from "wagmi";
 import { baseSepolia, mainnet, polygon, sepolia } from "wagmi/chains";
 
-const chains = [mainnet, sepolia, polygon, baseSepolia] as const;
-const transports = {
-  [mainnet.id]: http(),
-  [sepolia.id]: http(),
-  [polygon.id]: http(),
-  [baseSepolia.id]: http("https://sepolia.base.org"),
+const parameters = {
+  chains: [mainnet, sepolia, polygon, baseSepolia],
+  transports: {
+    [mainnet.id]: http(),
+    [sepolia.id]: http(),
+    [polygon.id]: http(),
+    [baseSepolia.id]: http("https://sepolia.base.org"),
+  },
 } as const;
 
-const config = createConfig({ chains, transports });
-const wagmiActionChainConfig = actionConfig({ chains, transports });
-
-export { config, wagmiActionChainConfig };
+export const config = createConfig(parameters);
+export const wagmiActionChainConfig = actionConfig(parameters);
