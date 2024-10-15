8291c0ec1a25fb69f875e7ec9cd7aaca8bece811
Justin Feng <realfengjia@foxmail.com>
2024-10-15 17:06:16 +0800

    chore: 🎸 use package version for swagger info json

--- apps/wallet-pass/src/_core/constant.ts
+++ apps/wallet-pass/src/_core/constant.ts
@@ -1,4 +1,5 @@
 import pino from 'pino';
+import packageJson from '../../package.json';
 import {env} from '../env';
 
 export const LOGGER = pino({level: env.LOG_LEVEL});
@@ -6,4 +7,5 @@ export const LOGGER = pino({level: env.LOG_LEVEL});
 export const API_INFO = {
   title: 'Token Kit Wallet Pass API',
   description: 'An API service to manage wallet pass.',
+  version: packageJson.version,
 };
